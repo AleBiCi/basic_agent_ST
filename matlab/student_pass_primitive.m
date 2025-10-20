c47916d3f094e2163cee9e3c9fc975151c41dc86
AleBiCi <alebianchiceriani@gmail.com>
2025-10-20 23:39:38 +0200

    Updated pass primitive

--- matlab/student_pass_primitive.m
+++ matlab/student_pass_primitive.m
@@ -8,5 +8,12 @@
 %%
 
 function [coeffsT2, v2, T2, coeffsT1, v1, T1] = student_pass_primitive(v0, a0, sf, vfmin, vfmax, Tmin, Tmax)
-% ...
+    Tvmin = 0.;
+    Tvmax = 0.;
+    if a0 >= 0
+        Tvmin = final_opt_time_pass(v0, a0, sf, vfmin);
+        Tvmax = final_opt_time_pass(v0, a0, sf, vfmax);
+    else
+        
+    end
 end
