75987cef0aec30912ad1327c49fe6222af0a7d13
AleBiCi <alebianchiceriani@gmail.com>
2025-10-22 11:09:35 +0200

    Finished pass primitive, untested

--- matlab/student_def_pass_primitive.m
+++ matlab/student_def_pass_primitive.m
@@ -10,7 +10,7 @@ final_opt_vel_pass_fun = matlabFunction(final_opt_vel_pass_var,'Vars',[v0,a0,sf,
 % - Use functions 'solve', 'diff', 'subs', and 'matlabFunction'
 
 %% Determine the optimal time to reach to final velocity
-sym z; % We create this variable for more stable solution of the equations
+syms z; % We create this variable for more stable solution of the equations
 
 final_opt_time_pass_var = 1./solve(diff(subs(total_cost_var, [af, T], [0., 1/z]), z) == 0, z);
 final_opt_time_pass_fun = matlabFunction(final_opt_time_pass_var,'Vars',[v0,a0,sf,vf],'File','final_opt_time_pass.m');
--- matlab/student_pass_primitive.m
+++ matlab/student_pass_primitive.m
@@ -10,10 +10,32 @@
 function [coeffsT2, v2, T2, coeffsT1, v1, T1] = student_pass_primitive(v0, a0, sf, vfmin, vfmax, Tmin, Tmax)
     Tvmin = 0.;
     Tvmax = 0.;
+    T_star = 0.;
+    v_star = 0.;
     if a0 >= 0
         Tvmin = final_opt_time_pass(v0, a0, sf, vfmin);
         Tvmax = final_opt_time_pass(v0, a0, sf, vfmax);
     else
-        
+        T_star = time_min_vel(v0, a0, sf, T);
+        v_star = min_vel(v0, a0, sf, T);
+        if v_star < v_min && v_min < v_max
+            Tvmin = final_opt_time_pass(v0, a0, sf, vfmin);
+            Tvmax = final_opt_time_pass(v0, a0, sf, vfmax);
+        elseif vfmin < v_star && v_star < vfmax
+            Tvmin = T_star;
+            Tvmax = final_opt_time_pass(v0, a0, sf, vfmax);
+        end
     end
+    T1 = 0.; T2 = 0.;
+    [T1, T2] = intersect([Tmin, Tmax],[Tvmax, Tvmin]);
+
+    if T1 > 0. && T1 <= T2
+        v1 = final_opt_vel_pass(v0, a0, sf, T2);
+        v2 = final_opt_vel_pass(v0, a0, sf, T1);
+        coeffsT1 = coef_list_fun(v0, a0, sf, v1, 0., T1);
+        coeffsT2 = coef_list_fun(v0, a0, sf, v2, 0., T2);
+    end
+
+    coeffsT1 = zeros(6);
+    coeffsT2 = zeros(6);
 end
